6504a3d4bd190e706a696c18cf67d36c888d55b8
Oleg Pimenov <pimenov.o@gmail.com>
2025-07-25 15:16:24 +0300

    feat: added ngx-vflow

--- apps/experiment-07/src/app/app.ts
+++ apps/experiment-07/src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from '../components/header';
+import { SchemaVisualizer } from '../components/schema-visualizer';
 
 @Component({
-    imports: [RouterModule, Header],
+    imports: [RouterModule, Header, SchemaVisualizer],
     selector: 'app-root',
     template: `
         <div class="flex min-h-svh flex-col">
             <app-header />
+            <schema-visualizer />
         </div>
     `
 })
